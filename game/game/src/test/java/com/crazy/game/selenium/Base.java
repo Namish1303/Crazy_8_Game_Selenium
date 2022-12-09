534bc9a9db29c03fdb9dd0457b1f130cd220fa4a
Namish Arora <namish.arora@gmail.com>
2022-12-09 09:46:34 -0500

    Deleted extra file

--- game/game/src/test/java/com/crazy/game/selenium/Base.java
+++ game/game/src/test/java/com/crazy/game/selenium/Base.java
@@ -1,11 +1,23 @@
 package com.crazy.game.selenium;
 
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class Base {
 
     @Before
-    public void setup()
-    {}
+    public WebDriver setup()
+    {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+
+
+
+
+
 
 }
--- game/game/src/test/java/com/crazy/game/selenium/Part1.java
+++ game/game/src/test/java/com/crazy/game/selenium/Part1.java
@@ -1,10 +1,16 @@
 package com.crazy.game.selenium;
 
 import java.time.Duration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ByIdOrName;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,27 +19,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-
-@SpringBootTest
-public class Part1 {
+public class Part1 extends Base{
 
     @Autowired
-    private WebDriver webDriver;
+    private WebDriver player1;
+
+    @Autowired
+    private WebDriver player2;
+
+    @Autowired
+    private WebDriver player3;
+
+    @Autowired
+    private WebDriver player4;
 
     @FindBy(id="connect")
     private WebElement connect;
+
+
+    @BeforeEach
+    void start()
+    {
+        WebDriverManager.chromedriver().setup();
+        player1 = new ChromeDriver();
+        player2 = new ChromeDriver();
+        player3 = new ChromeDriver();
+        player4 = new ChromeDriver();
+    }
 
     @Test
     void openBrowser()
     {
 
-        webDriver.navigate().to("http://localhost:8080");
-        //connect.click();
-        webDriver.findElement(By.className("connect"));
-        //connect.click();
+        player1.navigate().to("http://localhost:3000");
+        player1.findElement(By.id("username")).sendKeys("player1");
+        player1.findElement(By.id("connect")).click();
+
+        player2.navigate().to("http://localhost:3000");
+        player2.findElement(By.id("username")).sendKeys("player2");
+        player2.findElement(By.id("connect")).click();
+
+        player3.navigate().to("http://localhost:3000");
+        player3.findElement(By.id("username")).sendKeys("player3");
+        player3.findElement(By.id("connect")).click();
+
+        player4.navigate().to("http://localhost:3000");
+        player4.findElement(By.id("username")).sendKeys("player4");
+        player4.findElement(By.id("connect")).click();
+        
 
 
-        //System.out.println("HERE");
     }
 
+
+    @AfterEach
+    void end()
+    {
+        player1.close();
+        player2.close();
+        player3.close();
+        player4.close();
+    }
 }
