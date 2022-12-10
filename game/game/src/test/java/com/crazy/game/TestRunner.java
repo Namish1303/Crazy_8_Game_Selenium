c4ebd0008a4059c6bf3f9bbb397f0c0f29d4311a
Namish Arora <namish.arora@gmail.com>
2022-12-09 21:30:47 -0500

    Runner class

---
+++ game/game/src/test/java/com/crazy/game/TestRunner.java
@@ -0,0 +1,26 @@
+package com.crazy.game;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main(String[] args) {
+
+        int count=0;
+        Result result = JUnitCore.runClasses(TestSuite.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+            count+=1;
+        }
+
+        if(count == 0) {
+            System.out.println("\n\n\nAll tests Passed");
+        }
+        else
+        {
+            System.out.println("\n\n\nSome tests passed");
+        }
+    }
+}
